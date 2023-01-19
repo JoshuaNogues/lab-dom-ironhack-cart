6af114e564cb84b49e4586be22907f16730a0e41
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-01-19 08:55:43 -0500

    done w bonuses

--- src/index.js
+++ src/index.js
@@ -56,3 +56,59 @@ window.addEventListener('load', () => {
   })
 });
 
+function createProduct(e) {
+  const row = e.currentTarget.parentNode.parentNode
+  const name = row.querySelector("#name").value
+  const price = row.querySelector("#price").value
+  const table = document.getElementById("table")
+
+  let product = `
+  <tr class="product">
+  <td class="name">
+    <span>${name}</span>
+  </td>
+  <td class="price">$<span>${price}</span></td>
+  <td class="quantity">
+    <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  <td class="subtotal">$<span>0</span></td>
+  <td class="action">
+    <button class="btn btn-remove">Remove</button>
+  </td>
+</tr>
+  `
+
+  table.innerHTML += product
+
+  let removeButtons = table.querySelectorAll(".btn-remove")
+  
+  for (let i = 0; i < removeButtons.length; i++) {
+    removeButtons[i].addEventListener('click', removeProduct)
+  }
+
+  const newNameInput = document.getElementById('name')
+  const newPriceInput = document.getElementById('price')
+
+  newNameInput.value = ""
+  newPriceInput.value = ""
+ 
+
+  console.log("creating product", table)
+  //... your code goes here
+}
+
+window.addEventListener('load', () => {
+  const calculatePricesBtn = document.getElementById('calculate');
+  calculatePricesBtn.addEventListener('click', calculateAll);
+
+  let removeProducButtons = document.querySelectorAll(".btn-remove")
+
+  for (let i = 0; i < removeProducButtons.length; i++) {
+    removeProducButtons[i].addEventListener('click', removeProduct)
+  }
+
+  let createProductButton = document.getElementById("create")
+
+  createProductButton.addEventListener('click', createProduct)
+  //... your code goes here
+});
